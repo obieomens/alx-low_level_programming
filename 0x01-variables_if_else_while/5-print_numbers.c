9286d5677ca2d0533db3c1909583ae6cb83b6ca9
obieomens <obieomens@gmail.com>
2023-09-08 13:14:24 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Program that prints all single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+for (n = 0; n <= 9; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
