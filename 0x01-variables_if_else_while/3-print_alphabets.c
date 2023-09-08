aee8e217c47ca4c5977ce15f24c62b87acb751de
obieomens <obieomens@gmail.com>
2023-09-08 11:35:30 -0700

    3print-alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - program that prints small and capital letter a-z
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 97;
+	int m = 65;
+while (n <= 122)
+{
+	putchar(n);
+}
+while (m <= 90)
+{
+	putchar(m);
+}
+putchar("\n");
+return (0);
