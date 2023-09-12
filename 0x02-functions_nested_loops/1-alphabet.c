882c13c9501f7d551a535046224dd9a3eec5400a
obieomens <obieomens@gmail.com>
2023-09-12 07:54:54 -0700

    alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - programe that prints a-z
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+{
+	_putchar(c);
+}
+_putchar('\n');
+}
