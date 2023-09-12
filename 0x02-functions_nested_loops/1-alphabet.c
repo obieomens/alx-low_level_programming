856e97a81633b758509ef29da133a013326682fd
obieomens <obieomens@gmail.com>
2023-09-12 08:46:12 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * main - programe that prints a-z
+ * print_alphabet - programe that prints a-z
  * Return: void
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 char c;
 for (c = 'a'; c <= 'z'; c++)
