8b57a47c83e0067bc26ec309a28b27f6d6f2e04b
obieomens <obieomens@gmail.com>
2023-09-14 03:38:16 -0700

    correction on task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,15 +12,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("%d is Positive", n);
+printf("%d is positive", n);
 }
 else if (n == 0)
 {
-printf("%d is Zero", n);
+printf("%d is zero", n);
 }
 else
 {
-printf("%d is Negative", n);
+printf("%d is negative", n);
 }
 printf("\n");
 return (0);
--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,20 +5,18 @@
  */
 int main(void)
 {
-	int i, k;
+int i, k;
 for (i = 0; i <= 10; i++)
 {
 for (int k = i + 1; k <= 10; k++)
 {
 putchar(i + '0');
 putchar(k + '0');
-
 if (i != 8 || k != 9);
 {
-}
 putchar(',');
 putchar(' ');
-
+}
 }
 }
 putchar('\n');
