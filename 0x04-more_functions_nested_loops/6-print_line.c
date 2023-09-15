d12a83869ea9f14f7cb1967170daae841e0bb8ac
obieomens <obieomens@gmail.com>
2023-09-15 10:07:38 -0700

    06 print lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -15,10 +15,10 @@ void print_line(int n)
     else
     {
         int i;
-        for (i = 0; 1 < n; i++)
+        for (i = 0; i < n; i++)
 	{
-        	_putchar('_' + '0');
-       		_putchar('\n');
-	}
+        	_putchar('_');
+       	}
+	_putchar('\n');
     }
 }
