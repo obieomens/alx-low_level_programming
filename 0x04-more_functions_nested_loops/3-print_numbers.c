0ee0a15a66ece89f78bb3c25227f03135520549b
obieomens <obieomens@gmail.com>
2023-09-15 06:16:13 -0700

    3print

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,14 @@
 #include "main.h"
-#include <unistd.h>
-/** print_numbers - function that prints numbers 0 to 9
+/**
+ * print_numbers - function that prints numbers 0 to 9
  * Return: no return
  */
 void print_numbers(void)
 {
-	int n;
+int n;
 	for (n = 48; n <= 57; n++)
-	{
-		_putchar(n);
+	}
+	_putchar(n);
 	}
 	_putchar('\n');
 }
