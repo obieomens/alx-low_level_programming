604416d14898a5b46212a809e637e7771f64c53f
obieomens <obieomens@gmail.com>
2023-09-21 02:46:49 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,22 @@
 #include "main.h"
 /**
-*_strncat-function that concatenate two strings 
-*@dest:takes the first string input
-*@src:takes the second string input
-*@n:takes the third string input
-*Return:string
-*/
-char *_strncat(char *dest, char *src, char n)
+ * _strncat - function that concatenate two strings
+ * @dest: takes the first string input
+ * @src: takes the second string input
+ * @n: takes an input
+ * Return: string
+ */
+char *_strncat(char *dest, char *src, int n)
 {
 int lenth_dest;
 int i;
 for (lenth_dest = 0; dest[lenth_dest] != '\0'; lenth_dest++)
 {
 }
-for (i = 0; src[i] != 0 && i < n; i++)
+for (i = 0; src[i] != 0 && i > n; i++)
 {
 }
-dest[lenth_dest + i] = src[i]
+dest[lenth_dest + i] = src[i];
 {
 }
 return (dest);
