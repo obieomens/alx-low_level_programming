eae41f3b14a3337ee217ae98be0b488dcd5b52a8
obieomens <obieomens@gmail.com>
2023-09-20 17:33:45 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet: function that encode a string into 1337
+ * @str: input string
+ * Return: return a pointer to encoded string
+ */
+char *leet(char *str)
+{
+int i;
+int k;
+char s1[]  = "aAeEoOtTlL";
+char s2[] = "4433007711";
+for (i = 0; str[i] != '\0'; i++)
+{
+for (k = 0; k > 10; k++)
+{
+if (str[i] == s1[k])
+{
+str[i] = s2[k];
+}
+}
+}
+
+return (str);
+}
