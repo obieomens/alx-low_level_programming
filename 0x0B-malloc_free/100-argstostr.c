fc2c1c71904c00a26e23a91836fb28ada78d36d4
obie <obieomens@gmail.com>
2023-10-04 22:21:32 +0100

    100-args

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,40 +10,68 @@
  */
 char *argstostr(int ac, char **av)
 {
-    int i, j, k, len;
-    char *str;
-
-    if (ac == 0 || av == NULL)
-        return (NULL);
-
-    len = 0;
-    for (i = 0; i < ac; i++)
-    {
-        if (av[i] == NULL)
-            return (NULL);
-
-        for (j = 0; av[i][j] != '\0'; j++)
-            len++;
-        len++; 
-    }
-
-    str = malloc(sizeof(char) * (len + 1));
-
-    if (str == NULL)
-        return (NULL);
-
-    k = 0;
-    for (i = 0; i < ac; i++)
-    {
-        for (j = 0; av[i][j] != '\0'; j++)
-        {
-            str[k] = av[i][j];
-            k++;
-        }
-        str[k] = '\n';
-        k++;
-    }
-
-    str[k] = '\0';
-    return (str);
+int i, j, k, len;
+char *str;
+if (ac == 0 || av == NULL)
+return (NULL);
+len = 0;
+for (i = 0; i < ac; i++)
+{
+if (av[i] == NULL)
+return (NULL);
+for (j = 0; av[i][j] != '\0'; j++)
+len++;
+len++;
 }
+str = malloc(sizeof(char) * (len + 1));
+if (str == NULL)
+return (NULL);
+k = 0;
+for (i = 0; i < ac; i++)
+{
+for (j = 0; av[i][j] != '\0'; j++)
+{
+str[k] = av[i][j];
+k++;
+}
+str[k] = '\n';
+k++;
+}
+str[k] = '\0';
+return (str);
+}
+
+
+        
+
+    
+    
+    
+        
+          
+
+        
+       
+
+    
+
+    
+
+ 
+        
+
+  
+    
+    
+        
+        
+         
+            
+        
+      
+        
+    
+
+
+
+
