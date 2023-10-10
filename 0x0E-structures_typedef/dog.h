d3b1b53dc4be15a6fe7ae0a53ed425e989dee6f9
obie <obieomens@gmail.com>
2023-10-10 16:41:52 +0100

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,38 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * struct dog - Represents information about a dog
- * @name: name of the dog
- * @age: age of the dog
- * @owner: pointer to the owner of the dog
+ * @name: Pointer to the name of the dog (string)
+ * @age: Age of the dog (float)
+ * @owner: Pointer to the name of the owner (string)
+ *
+ * Description: This structure stores information about a dog,
+ * including its name, age, and owner's name.
  */
 struct dog
 {
 char *name;
-float *age;
+float age;
 char *owner;
 };
-#endif
 
+/**
+ * init_dog - Initializes a dog structure with provided values
+ * @d: Pointer to the struct dog to initialize
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Name of the owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
+#endif
