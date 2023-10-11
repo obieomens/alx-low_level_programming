6b9ebe017a0d57592cc1cd97aa87873f1b291124
obieomens <obieomens@gmail.com>
2023-10-11 11:25:49 -0700

    3-op_functions

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_op_function - select the option to perform arithematic op
+ * @s: operator argument
+ * Return: result of selected choice
+ */
+int (*get_op_func(char *s))(int, int)
+{
+op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i;
+/* initalize i */
+    i = 0;
+    /* s does not match any of the operator */
+    while (ops[i].op != NULL && *(ops[i].op) != *s)
+	    i++;
+    return (ops[i].f); /* return operator */
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - performs calculaator function on comand line
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: result of calc
+ */
+int main(int argc, char *argv[])
+{
+	char *op; /* operator */
+	int num1, num2; /* number input */
+	if (argc != 4) /* if argument count is wrong */
+{
+	printf("Error\n");
+	exit(98);
+}
+/* set cmd argument input */
+num1 = atoi(argv[1]);
+op = (argv[2]);
+num2 = atoi(argv[3]);
+if (get_op_func(op) == NULL || op[1] != '\0')
+{
+	printf("Error\n");
+	exit(99);
+}
+if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+{
+	printf("Error\n");
+	exit(100);
+}
+printf("%d\n", get_op_func(op)(num1, num2));
+return (0);
+}
